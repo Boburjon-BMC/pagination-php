25a12c5afe77bb60fcbd31adb98465278e4db472
Boburjon <abdunazarovbobur7777@gmail.com>
2023-08-15 21:38:42 +0500

    pagination php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Pagination php</title>
+   <link rel="stylesheet" href="web/css/style.css">
+</head>
+<body>
+   
+   
+
+   <script src="web/js/script.js"></script>
+</body>
+</html>
